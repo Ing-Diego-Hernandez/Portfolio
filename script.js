92e844a5114be3b15abbdbd37d399785ac0b21be
Diego <diegomaximiliano1203@gmail.com>
2024-08-16 22:58:09 -0600

    commit

--- script.js
+++
@@ -1,8 +0,0 @@
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-}
-
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    document.getElementById("form-success").style.display = "block";
-});
